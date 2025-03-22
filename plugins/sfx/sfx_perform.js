b5618ca8c800e33389b5de0a23c1b75ebfdec4d6
Kenny Perquy <perquy.k@gmail.com>
2025-03-22 12:49:07 +0100

    f

--- plugins/sfx/sfx_perform.js
+++ plugins/sfx/sfx_perform.js
@@ -23,5 +23,29 @@ function removeShells(){
 }
 
 function changeButtons(){
+    $("#save-in-between").html('<i class="fa fa-save" style="font-size: 17px"></i>');
+    $("#go-to-probleemmelder").attr("class", "btn red");
+    $("#go-to-probleemmelder").text("PM");
+    $("#go-to-mijntelenet").attr("class", "btn yellow");
+    $("#go-to-mijntelenet").text("MyTLN");
+}
 
+function addCustomerButtons() {
+    const antsURL = "http://ants.inet.telenet.be/tools/modems/modemtest#modem=";
+    const streetURL = "http://ants.inet.telenet.be/tools/nodes/livenodeping#node-input=";
+    const spotURL = "https://spot.prd.apps.telenet.be/care/customer/";
+    const nodeURL = "https://spot.prd.apps.telenet.be/care/network/";
+    
+    const injectTarget = $(".nav.navbar-nav");
+    
+
+    function navbarPrimaryButton(btnName, label){
+        const btnDiv = $("<div>").attr("style", "float: left;");
+        const btn = $("<button>").attr("class", "btn btn-dark").text(label).id(btnName);
+        btnDiv.append(btn);
+        return btnDiv;
+    }
+    function navbarSecondaryButton(btnName, label){
+
+    }
 }
